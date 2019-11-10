8cef0a53ee7b7e91bc87c076cd47095848c1eef3
Alejandro Sanchez <aalejo@gmail.com>
2019-11-10 15:57:44 +0000

    adding the index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<div style="text-align: center;">
+    <img src="https://ucarecdn.com/3a0e7d8b-25f3-4e2f-add2-016064b04075/rigobaby.jpg" />
+    <h2>Hello Geek, welcome to WordPress!</h2>
+    <p>If you see this screen, you probably have to run the installation script by typing:</p>
+    <p style="background: grey; color: white; font-size: 20px;">$ bash install.sh</p>
+    <small>Made with <span style="color: red;">♥️</span>at <a target="_blank" href="https://4geeksacademy.com">4Geeks Academy</a></small>
+</div>
