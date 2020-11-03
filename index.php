98259b922e9b027102f71c01d5e07e9b18fbe88e
Alejandro Sanchez <aalejo@gmail.com>
2020-11-03 01:10:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
-
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+<div style="text-align: center;">
+    <img src="https://github.com/4GeeksAcademy/wordpress-hello/blob/910c763722388c6bda785e89a12b79ba0694623b/rigo-baby.jpg?raw=true" />
+    <h2>Hello Geek, welcome to WordPress!</h2>
+    <p>If you see this screen, you probably have to run the installation script by typing:</p>
+    <p style="background: grey; color: white; font-size: 20px;">$ bash install.sh</p>
+  
+    <p><a target="_blank" href="https://www.loom.com/share/865fde325efa4aac9ccc473513a42d09">🐣 Need help? You can wath this video tutorial here.</a></p>
+  
+    <small>Made with <span style="color: red;">♥️</span>at <a target="_blank" href="https://4geeksacademy.com">4Geeks Academy</a></small>
+</div>
